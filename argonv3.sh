80c9e4b5bdc73cdd6b4a8631eb95d3406a5aa812
Nigel <thedjskywalker@gmail.com>
2025-02-02 02:21:10 +0100

    Update argonv3.sh

--- argonv3.sh
+++ argonv3.sh
@@ -89,7 +89,7 @@ if [ "$choice" = "2" ]; then
 fi
 
 # Ask if the user is using Argon V3 DAC
-printf "\e[1;32mAre you using Argon V3 DAC (y/n) ?\e"
+printf "\e[1;32mAre you using Argon V3 DAC (y/n) ?\e[0m"
 read use_dac
 
 # Apply settings based on selection
