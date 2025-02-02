2a3f798cab3a83eb02a4f0c5795475ec63b545cb
Nigel <thedjskywalker@gmail.com>
2025-02-02 01:07:50 +0100

    Update argonv3.sh

--- argonv3.sh
+++ argonv3.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Version 1.5.0
+# Version 1.6.0
 
 # Set default choices for the Argon version and DAC
 choice=1        # Default to Argon V3 normal
@@ -18,7 +18,7 @@ check_config() {
 # Function to apply configuration changes to config.txt
 apply_config() {
     echo "$1" >> /flash/config.txt
-    echo "Added $1 to /flash/config.txt."
+    printf "\e[1;32mAdded $1 to /flash/config.txt.\e[0m\n"
     config_changed=1
 }
 
@@ -35,7 +35,7 @@ check_eeprom_setting() {
 }
 
 # Ask the user which Argon version they are using
-echo "Are you using Argon V3 normal, or Argon V3 with NVMe?"
+printf "\e[1;32mAre you using Argon V3 normal, or Argon V3 with NVMe?\e[0m\n"
 echo "1) Argon V3 normal"
 echo "2) Argon V3 with NVMe"
 printf "Enter the number corresponding to your selection: "
@@ -43,25 +43,25 @@ read choice
 
 # Confirm the selection
 if [ "$choice" = "1" ]; then
-    echo "You selected Argon V3 normal."
+    printf "\e[1;32mYou selected Argon V3 normal.\e[0m\n"
 elif [ "$choice" = "2" ]; then
-    echo "You selected Argon V3 with NVMe."
+    printf "\e[1;32mYou selected Argon V3 with NVMe.\e[0m\n"
 else
-    echo "Invalid selection. Exiting."
+    printf "\e[1;31mInvalid selection. Exiting.\e[0m\n"
     exit 1
 fi
 
 printf "Is this correct? (y/n) "
 read confirm
 if [ "$confirm" != "y" ]; then
-    echo "Exiting. Please run the script again."
+    printf "\e[1;31mExiting. Please run the script again.\e[0m\n"
     exit 1
 fi
 
 # If the user selected Argon V3 with NVMe, ask for PCIe generation
 if [ "$choice" = "2" ]; then
     # Ask user for NVMe PCIe generation choice
-    echo "Select the PCIe generation for your NVMe drive:"
+    printf "\e[1;32mSelect the PCIe generation for your NVMe drive:\e[0m\n"
     echo "1) PCIe Gen 1"
     echo "2) PCIe Gen 2"
     echo "3) PCIe Gen 3"
@@ -79,7 +79,7 @@ if [ "$choice" = "2" ]; then
         pcie_param_line1="dtparam=nvme"
         pcie_param_line2="dtparam=pciex1_1=gen3"
     else
-        echo "Invalid selection. Exiting."
+        printf "\e[1;31mInvalid selection. Exiting.\e[0m\n"
         exit 1
     fi
 fi
@@ -99,7 +99,7 @@ if [ "$choice" = "1" ]; then
         if ! check_config "$setting"; then
             apply_config "$setting"
         else
-            echo "config.txt value $setting already present. Skipping..."
+            printf "\e[1;33mconfig.txt value $setting already present. Skipping...\e[0m\n"
         fi
     done
 elif [ "$choice" = "2" ]; then
@@ -111,7 +111,7 @@ elif [ "$choice" = "2" ]; then
         if ! check_config "$setting"; then
             apply_config "$setting"
         else
-            echo "config.txt value $setting already present. Skipping..."
+            printf "\e[1;33mconfig.txt value $setting already present. Skipping...\e[0m\n"
         fi
     done
 fi
@@ -121,7 +121,7 @@ if [ "$use_dac" = "y" ]; then
     if ! check_config "dtoverlay=hifiberry-dacplus,slave"; then
         apply_config "dtoverlay=hifiberry-dacplus,slave"
     else
-        echo "config.txt value dtoverlay=hifiberry-dacplus,slave already present. Skipping..."
+        printf "\e[1;33mconfig.txt value dtoverlay=hifiberry-dacplus,slave already present. Skipping...\e[0m\n"
     fi
 fi
 
@@ -137,24 +137,24 @@ if [ "$choice" = "1" ]; then
     if ! check_eeprom_setting "PSU_MAX_CURRENT=5000"; then
         eeprom_updates="$eeprom_updates\nPSU_MAX_CURRENT=5000"
     else
-        echo "EEPROM setting PSU_MAX_CURRENT=5000 already present. Skipping..."
+        printf "\e[1;33mEEPROM setting PSU_MAX_CURRENT=5000 already present. Skipping...\e[0m\n"
     fi
 elif [ "$choice" = "2" ]; then
     # Argon V3 with NVMe
     if ! check_eeprom_setting "BOOT_ORDER=0xf416"; then
         eeprom_updates="$eeprom_updates\nBOOT_ORDER=0xf416"
     else
-        echo "EEPROM setting BOOT_ORDER=0xf416 already present. Skipping..."
+        printf "\e[1;33mEEPROM setting BOOT_ORDER=0xf416 already present. Skipping...\e[0m\n"
     fi
     if ! check_eeprom_setting "PCIE_PROBE=1"; then
         eeprom_updates="$eeprom_updates\nPCIE_PROBE=1"
     else
-        echo "EEPROM setting PCIE_PROBE=1 already present. Skipping..."
+        printf "\e[1;33mEEPROM setting PCIE_PROBE=1 already present. Skipping...\e[0m\n"
     fi
     if ! check_eeprom_setting "PSU_MAX_CURRENT=5000"; then
         eeprom_updates="$eeprom_updates\nPSU_MAX_CURRENT=5000"
     else
-        echo "EEPROM setting PSU_MAX_CURRENT=5000 already present. Skipping..."
+        printf "\e[1;33mEEPROM setting PSU_MAX_CURRENT=5000 already present. Skipping...\e[0m\n"
     fi
 fi
 
@@ -163,17 +163,17 @@ if [ -n "$eeprom_updates" ]; then
     echo -e "$eeprom_updates" | grep -v '^$' > /tmp/boot.conf
     rpi-eeprom-config -a /tmp/boot.conf
     rm /tmp/boot.conf
-    echo "EEPROM updated with new settings."
+    printf "\e[1;32mEEPROM updated with new settings.\e[0m\n"
     eeprom_changed=1
 else
-    echo "No EEPROM updates necessary."
+    printf "\e[1;32mNo EEPROM updates necessary.\e[0m\n"
 fi
 
 rm /tmp/current_eeprom.conf
 
 # Determine if a reboot is needed
 if [ "$config_changed" -eq 1 ] || [ "$eeprom_changed" -eq 1 ]; then
-    echo "A reboot is required for changes to take effect."
+    printf "\e[1;32mA reboot is required for changes to take effect.\e[0m\n"
     printf "Do you want to reboot now? (y/n) "
     read reboot_confirm
     if [ "$reboot_confirm" = "y" ]; then
@@ -183,5 +183,4 @@ if [ "$config_changed" -eq 1 ] || [ "$eeprom_changed" -eq 1 ]; then
         echo "Reboot skipped. Please remember to reboot your Raspberry Pi later."
     fi
 else
-    echo "No changes were made. Reboot is not required."
-fi
+    printf "\e
